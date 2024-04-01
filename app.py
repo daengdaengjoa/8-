3f57758a87779430503450dfc2c1caddca6d1539
김준수 <rich7520@naver.com>
2024-04-01 19:46:25 +0900

    작성글 라우팅

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from datetime import datetime
+
 app = Flask(__name__)
 
-## URL 별로 함수명이 같거나,
-## route('/') 등의 주소가 같으면 안됩니다.
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route("/")
+def index():
+    return render_template("게시글 작성.html")
 
-@app.route('/mypage')
-def mypage():
-	return 'This is My page!'
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    author = request.form["author"]
+    movie = request.form["movie"]
+    title = request.form["title"]
+    content = request.form["content"]
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # 여기서 데이터베이스에 데이터를 저장
+    print("작성 시간:", current_time)
+    print("작성자:", author)
+    print("영화 이름:", movie)
+    print("제목:", title)
+    print("내용:", content)
+    return redirect(url_for("index"))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
