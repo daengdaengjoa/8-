dad6a023fbdf2e1b519f67f235cd52e45ade152e
Billyhyunjun <phjphj9999@gmail.com>
2024-04-02 17:10:58 +0900

    검색 기능 추가

--- app.py
+++ app.py
@@ -1,134 +1,119 @@
-from flask import Flask, render_template, request, redirect, url_for
+import os
 from datetime import datetime
-import DB_app
-# from flask_sqlalchemy import SQLAlchemy
-# import os
-# from flask_paginate import Pagination, get_page_args
+
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
 
 
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'database.db')
+
+db = SQLAlchemy(app)
+
+
+class UserInfo(db.Model):  # 유저 정보 데이터
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String, nullable=False)
+    pw = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    age = db.Column(db.String, nullable=False)
+    gender = db.Column(db.String, nullable=False)
+    area = db.Column(db.String, nullable=False)
+
+
+class Posting(db.Model):  # 게시글 정보 데이터
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+    movie_title = db.Column(db.String, nullable=False)
+    posting_title = db.Column(db.String, nullable=False)
+    review = db.Column(db.String, nullable=False)
+    grade = db.Column(db.Float, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+
+
+class Comment(db.Model):  # 댓글 정보 데이터
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.String, nullable=False) # 이거 나중에 추가 되어야 함
+    user_id = db.Column(db.String, nullable=False)
+    detail = db.Column(db.String, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
 
 
 @app.route("/")
 def 메인화면():
-    return render_template("메인화면.html")
+    posts = Posting.query.order_by(desc(Posting.date)).limit(3).all()
+    return render_template("메인화면.html", posts=posts)
 
-@app.route("/게시글작성")
+
+@app.route("/게시글작성", methods=["GET", "POST"])
 def 게시글작성():
+    # 글작성의 내용을 입력하고 작성 완료를 누르면 동작
+    if request.method == "POST":
+        # Posting테이블의 칼럼에 맞추어 변수의 값 입력
+        new_Posting = Posting(
+            user_id='123', # 아직 유저 정보 없음....
+            username=request.form['username'],
+            movie_title=request.form['movie_title'],
+            posting_title=request.form['posting_title'],
+            review=request.form['review'],
+            grade='3',  # 데이터 반영 필요
+            date=datetime.now()
+        )
+        # 데이터베이스 세션에 추가
+        db.session.add(new_Posting)
+
+        # 변경 사항 커밋
+        db.session.commit()
+    # 데이터 값 저장 만하고 보여줄 필요는 없으니 리턴 값 없음
     return render_template("게시글 작성.html")
+
 
 @app.route("/전체글조회", methods=["POST", "GET"])
 def 전체글조회():
-    # cur = get_cur()  # DB와 연결
-    per_page = 10  # 게시글 10개 씩
-    # page, _, offset = get_page_args(per_page=per_page)
-    print(request.method)
-
-    # POST request라면,
+    # POST 입력을 받고 왔으면 조건문 입장
     if request.method == "POST":
-        # query = request.form["find"]  # 전체글 조회에서 입력칸에서 받은 "find"글자
-        # tag = request.form["tag"]  # 전체글 조회에서 검색방법으로 선택한 "tag"글자
-        # query_for_like = ("%" + query + "%").lower()  # 검색 편의를 위해 소문자로 변환
-        # 
-        # # 게시글 수 파악
-        # cur.execute(
-        #     "SELECT COUNT(*) FROM 테이블명 "
-        #     "WHERE %s LIKE %s;",
-        #     (tag, query_for_like),  # 포스트 제목과 내용도 소문자 변환해서 검색
-        # )
-        # total = cur.fetchone()[0]  #  데이터베이스에서 가져온 결과 집합의 첫 번째 행에서 첫 번째 열의 값
-        # 
-        # # 게시글 내용
-        # cur.execute(
-        #     "SELECT p.아이디, 내용, 제목, 날짜, 영화이름, 리뷰, 평점, 이름 "
-        #     "FROM 게시글DB p JOIN 유저DB u ON p.아이디 = u.아이디 "
-        #     "WHERE %s LIKE %s "
-        #     "ORDER BY created DESC;",  # POST라면 LIKE 검색 결과를 보여줌
-        #     (tag, query_for_like),
-        # )
-        total = 1
-        posts = [{
-            "id": "이름",
-            "content": "내용",
-            "date": "날짜",
-            "title": "제목",
-            "movie": "영화이름",
-            "review": "리뷰",
-            "score": "2",
-            "name": "이름",
-        }]
+        # post로 가져온 매개변수를 각각 넣어준다.
+        find = request.form.get("find")
+        tag = request.form.get("tag")
+        # Posting 테이블에서 tag=검색 조건에 해당하는 칼럼에서 find의 내용을 포함하는 값이 있다면 모두 가져온다.
+        posts = Posting.query.filter(getattr(Posting, tag).like(f"%{find}%")).all()
 
-    # POST가 아니라면, 즉 GET request라면,
+    # 검색기능을 하지 않고 처음들어올 때에는 모든 게시글이 보일 수 있도록 한다.
     else:
-        # cur.execute("SELECT COUNT(*) FROM 게시글DB p;")  # 게시글 수 파악
-        # total = cur.fetchone()[0]
-        # 
-        # cur.execute(  # GET이라면 검색 없이 모든 포스트를 보여줌
-        #     "SELECT p.아이디, 내용, 제목, 날짜, 영화이름, 리뷰, 평점, 이름 "
-        #     "FROM 게시글DB p JOIN 유저DB u ON p.아이디 = u.아이디 "
-        #     "ORDER BY 날짜 DESC;",  # 생산 일자 역순으로
-        #     (per_page, offset),
-        # )
-        total = 1
-        posts = [{
-            "id": "billy",
-            "content": "content",
-            "date": "date",
-            "title": "title",
-            "movie": "movie",
-            "review": "review",
-            "score": "score",
-            "name": "name",
-        },{
-            "id": "billy",
-            "content": "content",
-            "date": "date",
-            "title": "title",
-            "movie": "movie",
-            "review": "review",
-            "score": "score",
-            "name": "name",
-        },]
-    print(posts)
-    # 모든 데이터 베이스 값 딕셔너리화 하기
-    # posts = cur.fetchall()
+        posts = Posting.query.all()
 
-    return render_template("전체글 조회.html",
-                            posts=posts, # 게시글 내용
-                            # pagination=Pagination(
-                            # page=page,  # 지금 우리가 보여줄 페이지는 1 또는 2, 3, 4, ... 페이지인데,
-                            # total=total,  # 총 몇 개의 포스트인지를 미리 알려주고,
-                            # per_page=per_page  # 한 페이지당 몇 개의 포스트를 보여줄지 알려주고,
-                            # )
-    )
+    return render_template("전체글 조회.html", posts=posts)  # 게시글 내용
+
 
 @app.route("/게시글조회", methods=["POST", "GET"])
 def 게시글조회():
+    # 전체글에서 게시글을 클릭해서 들어올 때에 해당하는 게시글에 맞추어서 글과 댓글을 가져올 수 있도록
+    post_id = request.args.get("post_id")
+    # 게시글 아이디에 맞추어 글의 내용을 가져온다.
+    posts = Posting.query.filter_by(id=post_id).first()
 
-    # # 댓글 로딩
-    # cur = get_cur()
-    # title = request.form["title"]
-    # date = request.form["date"]
-    # cur.execute(
-    #     "SELECT p.아이디, 댓글내용, 날짜, 이름 "
-    #     "FROM 댓글 DB p JOIN 유저DB u ON p.아이디 = u.아이디 "
-    #     "WHERE 제목 = %s AND 날짜 = %s"
-    #     "ORDER BY 날짜 DESC;",
-    #     (title, date),# 생산 일자 역순으로
-    # )
+    # POST시 댓글 저장한다.
+    if request.method == "POST":
+        new_Comment = Comment(
+            post_id=post_id,
+            user_id="심청이",
+            detail=request.form['detail'],
+            date=datetime.now()
+        )
+        # 데이터베이스 세션에 추가
+        db.session.add(new_Comment)
 
-    # 모든 데이터 베이스 값 딕셔너리화 하기
-    # comment = cur.fetchall()
+        # 변경 사항 커밋
+        db.session.commit()
+    # 게시글 내용 표시
+    comments = Comment.query.filter_by(post_id=post_id).all()
 
-    comment = {
-        "id": "billy",
-        "content": "content",
-        "date": "date",
-        "name": "name"
-    }
+    return render_template("게시글 조회.html", comments=comments, posts=posts)
 
-    return render_template("게시글 조회.html", comment=comment)
 
 @app.route("/submit", methods=["POST"])
 def submit():
@@ -136,7 +121,7 @@ def submit():
     movie = request.form["movie"]
     title = request.form["title"]
     content = request.form["content"]
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    current_time = datetime.now()
 
     # 여기서 데이터베이스에 데이터를 저장
     print("작성 시간:", current_time)
@@ -149,3 +134,6 @@ def submit():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+with app.app_context():
+    db.create_all()
