b6bcd715795c372d8d9d7673a6019effc7a6964a
soyeongpark2090 <syp4546@gmail.com>
2024-04-02 13:04:17 +0900

    게시글 작성 db에 저장, 게시글 작성.html input란 추가

--- DB_app.py
+++ DB_app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for
 import os
 from flask_sqlalchemy import SQLAlchemy
 
@@ -23,7 +23,6 @@ class UserInfo(db.Model):
 class Posting(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.String, nullable=False)
-    username = db.Column(db.String, nullable=False)
     movie_title = db.Column(db.String, nullable=False)
     posting_title = db.Column(db.String, nullable=False)
     review = db.Column(db.String, nullable=False)
@@ -36,6 +35,30 @@ class Comment(db.Model):
     user_id = db.Column(db.String, nullable=False)
     detail = db.Column(db.String, nullable=False)
     date = db.Column(db.DateTime, nullable=False)
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    user_id_receive = request.form.get("author")
+    movie_title_receive = request.form.get("movie_title")
+    posting_title_receive = request.form.get("posting_title")
+    review_recieve = request.form.get("review")
+    grade_receive = request.form.get("grade")
+    date_receive = request.form.get("date")
+
+    posting = Posting(author=user_id_receive, movie_title=movie_title_receive,
+                      posting_title=posting_title_receive, review=review_recieve, grade=grade_receive, date=date_receive)
+    db.session.add(posting)
+    db.session.commit()
+
+    #     # 여기서 데이터베이스에 데이터를 저장
+    # print("작성 시간:", current_time)
+    # print("작성자:", author)
+    # print("영화 이름:", movie)
+    # print("제목:", title)
+    # print("내용:", content)
+    # return redirect(url_for("게시글작성"))
+    return redirect(url_for('게시글 작성'))
 
 
 with app.app_context():
--- app.py
+++ app.py
@@ -8,33 +8,20 @@ app = Flask(__name__)
 def 메인화면():
     return render_template("메인화면.html")
 
+
 @app.route("/게시글작성")
 def 게시글작성():
     return render_template("게시글 작성.html")
+
 
 @app.route("/전체글조회")
 def 전체글조회():
     return render_template("전체글 조회.html")
 
+
 @app.route("/게시글조회")
 def 게시글조회():
     return render_template("게시글 조회.html")
-
-@app.route("/submit", methods=["POST"])
-def submit():
-    author = request.form["author"]
-    movie = request.form["movie"]
-    title = request.form["title"]
-    content = request.form["content"]
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    # 여기서 데이터베이스에 데이터를 저장
-    print("작성 시간:", current_time)
-    print("작성자:", author)
-    print("영화 이름:", movie)
-    print("제목:", title)
-    print("내용:", content)
-    return redirect(url_for("게시글작성"))
 
 
 if __name__ == "__main__":
