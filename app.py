5192412fc1aaf6fba12be85b046cb145a74bbd0f
Billyhyunjun <phjphj9999@gmail.com>
2024-04-02 17:44:18 +0900

    검색 기능 추가

--- app.py
+++ app.py
@@ -137,24 +137,6 @@ def 게시글조회():
     return render_template("게시글 조회.html", data=data1)
 
 
-
-@app.route("/submit", methods=["POST"])
-def submit():
-    author = request.form["author"]
-    movie = request.form["movie"]
-    title = request.form["title"]
-    content = request.form["content"]
-    current_time = datetime.now()
-
-    # 여기서 데이터베이스에 데이터를 저장
-    print("작성 시간:", current_time)
-    print("작성자:", author)
-    print("영화 이름:", movie)
-    print("제목:", title)
-    print("내용:", content)
-    return redirect(url_for("게시글작성"))
-
-
 if __name__ == "__main__":
     app.run(debug=True)
 
